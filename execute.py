a9b153314439d25e5160434a72260497a4b3438a
DiamondGotCat <124330624+DiamondGotCat@users.noreply.github.com>
2024-08-03 14:15:28 +0900

    Update execute.py

--- execute.py
+++ execute.py
@@ -19,7 +19,7 @@ inputs = tokenizer(prompt, return_tensors="pt")
 # テキスト生成
 outputs = model.generate(
     inputs.input_ids, 
-    max_length=50,  # 生成する最大トークン数
+    max_length=100,  # 生成する最大トークン数
     num_return_sequences=1,  # 生成するシーケンスの数
     pad_token_id=tokenizer.eos_token_id  # パディングトークンIDを設定
 )
@@ -29,5 +29,5 @@ generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 result = generated_text[len(prompt):]
 
 # 結果を表示
-print(f"your prompt: {prompt}")
-print(f"generated: {result}")
+print(f"prompt: {prompt}")
+print(f"result: {result}")
