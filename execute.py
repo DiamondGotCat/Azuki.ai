37d7ddea562a9c3d0452d8cc0bf15d6092f5113e
DiamondGotCat <chii.2019.5555@gmail.com>
2024-11-24 23:13:36 +0900

    Update execute.py

--- execute.py
+++ execute.py
@@ -10,24 +10,26 @@ model = GPT2LMHeadModel.from_pretrained(model_save_path)
 if tokenizer.pad_token is None:
     tokenizer.pad_token = tokenizer.eos_token
 
-# テキスト生成のためのプロンプト
-prompt = input("prompt: ")
+while True:
 
-# トークナイズ
-inputs = tokenizer(prompt, return_tensors="pt")
+    # テキスト生成のためのプロンプト
+    prompt = input("prompt: ")
 
-# テキスト生成
-outputs = model.generate(
-    inputs.input_ids, 
-    max_length=100,  # 生成する最大トークン数
-    num_return_sequences=1,  # 生成するシーケンスの数
-    pad_token_id=tokenizer.eos_token_id  # パディングトークンIDを設定
-)
+    # トークナイズ
+    inputs = tokenizer(prompt, return_tensors="pt")
 
-# 生成されたテキストをデコード
-generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-result = generated_text[len(prompt):]
+    # テキスト生成
+    outputs = model.generate(
+        inputs.input_ids, 
+        max_length=100,  # 生成する最大トークン数
+        num_return_sequences=1,  # 生成するシーケンスの数
+        pad_token_id=tokenizer.eos_token_id  # パディングトークンIDを設定
+    )
 
-# 結果を表示
-print(f"prompt: {prompt}")
-print(f"result: {result}")
+    # 生成されたテキストをデコード
+    generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    result = generated_text[len(prompt):]
+
+    # 結果を表示
+    print(f"prompt: {prompt}")
+    print(f"result: {result}")
