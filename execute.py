a64fb823193f9976eaa4ea693574d03956d9cfc5
DiamondGotCat <chii.2019.5555@gmail.com>
2024-12-10 16:06:45 +0900

    Update execute.py

--- execute.py
+++ execute.py
@@ -1,12 +1,10 @@
 import torch
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
-# モデルとトークナイザーのロード
 model_save_path = "./trained_model"
 tokenizer = GPT2Tokenizer.from_pretrained(model_save_path)
 model = GPT2LMHeadModel.from_pretrained(model_save_path)
 
-# パディングトークンの設定（必要な場合）
 if tokenizer.pad_token is None:
     tokenizer.pad_token = tokenizer.eos_token
 
@@ -14,24 +12,20 @@ prompt = ""
 
 while True:
 
-    # テキスト生成のためのプロンプト
     prompt += "<user>" + input("> ") + "</user>"
 
     print("---")
 
-    # トークナイズ
     inputs = tokenizer(prompt, return_tensors="pt")
 
-    # テキスト生成
     outputs = model.generate(
         inputs.input_ids,
         attention_mask=inputs.attention_mask,
-        max_length=4096,  # 生成する最大トークン数
-        num_return_sequences=1,  # 生成するシーケンスの数
-        pad_token_id=tokenizer.eos_token_id  # パディングトークンIDを設定
+        max_length=4096,
+        num_return_sequences=1,
+        pad_token_id=tokenizer.eos_token_id
     )
 
-    # 生成されたテキストをデコード
     generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
     result = generated_text[len(prompt):].replace("<assistant>", "").replace("</assistant>", "").replace("<user>", "").replace("</user>", "")
 
